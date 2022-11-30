bbac00c4d66f36b20ec414315e6e6f949482d281
Jorge Gabriel Chagas Galvão <gabriel.ggalvao@mercadolivre.com>
2022-11-30 13:14:31 -0300

    Criando os 4 métodos (getByNome, post, put e delete) na classe CategoriaRepository

--- src/main/java/com/projetooka/projetooka/repository/CategoriaRepository.java
+++ src/main/java/com/projetooka/projetooka/repository/CategoriaRepository.java
@@ -2,8 +2,13 @@ package com.projetooka.projetooka.repository;
 
 import com.projetooka.projetooka.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    public List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 
 }
