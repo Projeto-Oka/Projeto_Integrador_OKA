38f9a3026a6846d5db07d89f7bde6311c878ee5c
Guilherme A. Silva <guilherme.salmeida@mercadolivre.com>
2022-12-01 13:51:34 -0300

    Criação da classe CategoriController, metodos do CRUD e adição de getters e setters

--- src/main/java/com/projetooka/projetooka/model/Produto.java
+++ src/main/java/com/projetooka/projetooka/model/Produto.java
@@ -42,4 +42,60 @@ public class Produto {
 	@ManyToOne
 	@JsonIgnoreProperties("produto")
 	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 }
--- src/main/java/com/projetooka/projetooka/model/Usuario.java
+++ src/main/java/com/projetooka/projetooka/model/Usuario.java
@@ -1,38 +1,85 @@
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+package com.projetooka.projetooka.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
 @Table(name="tb_usuario")
 public class Usuario {
-	
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private Long id;
 
-@NotBlank(message = "Deve conter um nome de usuário")
-private String nome;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-@NotBlank(message = "Deve conter um email")
-@Email
-private String email;
+    @NotBlank(message = "Deve conter um nome de usuário")
+    private String nome;
 
-
-@NotBlank(message = "Deve conter no minimo 8 e no maximo 16 caracteres")
-@Size(min = 8,max = 16)
-private String senha;
-
-private String foto;
+    @NotBlank(message = "Deve conter um email")
+    @Email
+    private String email;
 
 
-}
+    @NotBlank(message = "Deve conter no minimo 8 e no maximo 16 caracteres")
+    @Size(min = 8, max = 16)
+    private String senha;
+
+    private String foto;
 
 
-@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
     @JsonIgnoreProperties("categoria")
     private List<Produto> produtos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+}
